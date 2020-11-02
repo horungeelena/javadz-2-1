67d3ef6868d5c54c673fca241f1fd8d08c1239e5
Elena Fedoseeva <horungeelena@gmail.com>
2020-11-02 14:21:38 +0200

    correction of code and amounts

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         boolean debit = true;
-        int start = 100;
-        int sum;
+        int start = 300;
+        long sum;
         if (debit) {
-            sum = 1100;
+            sum = 1500;
         } else {
-            sum = 100;
+            sum = 900;
         }
-        long result = start + sum + (sum /100);
+        long result = start + sum + (sum / 100);
         long limit = 1000;
         if ( result >= limit); {
         }
