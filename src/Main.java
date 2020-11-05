25e128d3c458d94a04afe22b8abd165f673aa9ae
Elena Fedoseeva <horungeelena@gmail.com>
2020-11-05 14:22:45 +0200

    rewritten code with specified limits

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         boolean debit = true;
-        int start = 300;
         long sum;
         if (debit) {
             sum = 1500;
         } else {
-            sum = 900;
+            sum = 300;
         }
-        long result = start + sum + (sum / 100);
+        long bonus = sum / 100 ;
         long limit = 1000;
-        if ( result >= limit); {
+        if (sum <= limit) {
+            bonus = 0;
         }
-        System.out.println(result);
+        System.out.println(bonus);
     }
 }
